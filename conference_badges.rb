2eeeae16dd88cad4d57d1642a32331d235c5cae0
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 17:57:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr2 = []
+  arr.each do |elem|
+    str = arr[i]
+    arr2 << "Hello, my name is #{str}."
+  end
+  return arr
+end
+
+
+
+def test 
+  arr = ["fhdsuji","fdshjk"]
+  arr.each do |n|
+    puts n
+  end
+end
+
+test
+print(batch_badge_creator(["tom", "jerry"]))
+
